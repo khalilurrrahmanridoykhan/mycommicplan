62c8bc06f48708c07024275fcc8ceaab5ae7b964
Khalilur Rahman Ridoy Khan <114473324+khalilurrrahmanridoykhan@users.noreply.github.com>
2025-02-07 09:19:34 +0600

    create project

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,15 @@
+# backend/api/views.py
 from django.shortcuts import render
-
 from rest_framework import viewsets
-from .models import Form, Submission
-from .serializers import FormSerializer, SubmissionSerializer
+from .models import Form, Submission, Project
+from .serializers import FormSerializer, SubmissionSerializer, ProjectSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.contrib.auth import authenticate
 from rest_framework import status
+
 class FormViewSet(viewsets.ModelViewSet):
     queryset = Form.objects.all()
     serializer_class = FormSerializer
@@ -28,3 +29,7 @@ class CustomAuthToken(APIView):
             token, created = Token.objects.get_or_create(user=user)
             return Response({'token': token.key})
         return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
